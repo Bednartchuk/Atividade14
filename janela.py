d5b5bc783a2aa992cbed8819f33a0b5b63c58b67
Bednartchuk <125326592+Bednartchuk@users.noreply.github.com>
2023-09-22 19:50:14 -0300

    Atualizar o janela.py

---
+++ janela.py
@@ -0,0 +1,70 @@
+import sys 
+ from PySide6.QtWidgets import QPushButton, QMainWindow, QWidget, QApplication, QLabel, QLineEdit, QTextEdit, QVBoxLayout 
+  
+ class MainJanela(QMainWindow): 
+     def __init__(self, parent=None): 
+         super().__init__(parent) 
+  
+         self.conjunto_widget = QWidget() 
+         self.v_layout = QVBoxLayout() 
+         self.conjunto_widget.setLayout(self.v_layout) 
+  
+         self.label1 = QLabel("Texto aqui...") 
+         self.v_layout.addWidget(self.label1) 
+         self.setCentralWidget(self.conjunto_widget) 
+  
+     def atualizar_label(self, nome, mensagem): 
+  
+         texto = f"NOME: {nome}\nMENSAGEM: {mensagem}" 
+         self.label1.setText(texto) 
+  
+ class NovaJanela(QWidget): 
+     def __init__(self, main_window): 
+         super().__init__() 
+         self.main_window = main_window 
+         self.initUI() 
+  
+     def initUI(self): 
+         self.setWindowTitle("TESTE 1") 
+         self.setGeometry(100, 100, 300, 200) 
+  
+         #1ª Caixa de Mensagem 
+         self.nomeLabel = QLabel("TESTE 2") 
+         self.nomeInput = QLineEdit() 
+  
+         #2ª Caixa de Mensagem 
+         self.mensagemLabel = QLabel("TESTE 3") 
+         self.mensagemInput = QTextEdit() 
+  
+         #Botão para Enviar 
+         self.botaoEnviar = QPushButton("TESTE 4") 
+         self.botaoEnviar.clicked.connect(self.enviar_msg) 
+  
+         layout = QVBoxLayout() 
+         layout.addWidget(self.nomeLabel) 
+         layout.addWidget(self.nomeInput) 
+         layout.addWidget(self.mensagemLabel) 
+         layout.addWidget(self.mensagemInput) 
+         layout.addWidget(self.botaoEnviar) 
+         self.setLayout(layout) 
+  
+     def enviar_msg(self): 
+  
+         nome = self.nomeInput.text() 
+         mensagem = self.mensagemInput.toPlainText() 
+  
+         self.main_window.atualizar_label(nome, mensagem) 
+         self.close() 
+  
+ if __name__ == '__main__': 
+     app = QApplication(sys.argv) 
+     main_window = MainJanela() 
+     main_window.setWindowTitle("Título aqui!!") 
+     main_window.show() 
+  
+     #Escolhendo a cor de fundo 
+     novaJanela = NovaJanela(main_window) 
+     novaJanela.setStyleSheet("background-color: gray;") 
+     novaJanela.show() 
+  
+     sys.exit(app.exec())
